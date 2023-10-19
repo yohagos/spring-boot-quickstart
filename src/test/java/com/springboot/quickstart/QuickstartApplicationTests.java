d1d7b5fc98a033d8850af3fba2867b3fcda5cbdc
yohagos <yosef.hagos@googlemail.com>
2023-10-19 13:45:20 +0200

    init project

---
+++ src/test/java/com/springboot/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
